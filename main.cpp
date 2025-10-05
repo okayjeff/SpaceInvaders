23c2f39023b8d0dcaad3d94138e9b6b2fea68c36
okayjeff <jg@okayjeff.com>
2025-10-05 12:21:37 -0700

    delete unused code

--- main.cpp
+++ main.cpp
@@ -1,34 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
-#include <regex>
 #include "Game.h"
-
-
-// void handleFire(const Player &player,
-//                 std::vector<std::unique_ptr<Entity>> &entities,
-//                 sf::RenderWindow &window) {
-//   entities.erase(std::remove_if(entities.begin(), entities.end(),
-//                                 [](const auto &bullet) {
-//                                   return bullet->getPosition().y < -10.f;
-//                                 }),
-//                  entities.end());
-//
-//   static sf::Clock bulletClock;
-//   if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space) &&
-//       bulletClock.getElapsedTime().asMilliseconds() > fireRate) {
-//     entities.push_back(std::make_unique<Enemy>(player.getPosition()));
-//     bulletClock.restart();
-//   }
-// }
-
-
-// void spawnEnemies(std::vector<std::unique_ptr<Entity>> &entities) {
-//   constexpr int padding{120};
-//   for (int i = 1; i < static_cast<int>(screenWidth / padding); ++i) {
-//     entities.push_back(std::make_unique<Enemy>(sf::Vector2f{
-//             static_cast<float>(padding * i), static_cast<float>(padding)}));
-//   }
-// }
 
 
 int main() {
